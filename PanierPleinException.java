d8033f9f7e134644e786540a7668b46fee94be44
Roudet <Celine.Roudet@u-bourgogne.fr>
2020-09-15 11:52:35 +0200

    premier commit

---
+++ PanierPleinException.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author roudet
+ */
+public class PanierPleinException extends Exception {
+    
+    public PanierPleinException()
+    {
+	super("Ajout impossible car le panier est plein !");
+    }
+
+}
